b1b582123ed80d2be457caa25c691ef1bf520ab6
fifem1518 <90783281+fifem1518@users.noreply.github.com>
2021-09-15 11:30:07 -0400

    First python upload

---
+++ P1LAB1_Fife.py
@@ -0,0 +1,10 @@
+# CTI 110
+# P1T2 - Salary Calculator
+# Fife Marshall
+# 9/15/21
+
+
+userNum = int(input())
+userNumSquared = userNum * userNum   # Bug here; fixed
+   
+print(userNumSquared)       # Output formatting issue here; fixed
